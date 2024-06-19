abd7b7a8298324067cd3edc37ba9844c0de9c820
Miguel Urena <urena_miguel82@outlook.com>
2024-06-19 11:54:05 -0700

    Add app/projects/page.js

---
+++ app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center py-2">
+      <h1 className="text-6xl font-bold">My Projects</h1>
+      <p className="mt-3 text-2xl">This is the projects page.</p>
+    </main>
+  )
+}
