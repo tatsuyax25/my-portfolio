37f35bc614937e53f448dfe4a1de46f7fda0bfb9
Miguel Urena <urena_miguel82@outlook.com>
2024-06-19 11:56:07 -0700

    Add app/contact/page.js

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center py-2">
+      <h1 className="text-6xl font-bold">Contact Me</h1>
+      <p className="mt-3 text-2xl">This is the contact page.</p>
+    </main>
+  )
+}
