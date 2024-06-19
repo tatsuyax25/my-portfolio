283409da4359e50a2184a1b4c9be7471c577eb9b
Miguel Urena <urena_miguel82@outlook.com>
2024-06-19 11:46:29 -0700

    Add app/about/page.js

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center py-2">
+      <h1 className="text-4xl font-bold">About Me</h1>
+      <p className="mt-3 text-2xl">This is the about page.</p>
+    </main>
+  )
+}
