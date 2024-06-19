25e44e7155d3fb5d6effdc609ee3f5e901b043c5
Miguel Urena <urena_miguel82@outlook.com>
2024-06-19 11:51:02 -0700

    Add app/skills.page.js

---
+++ app/skills/page.js
@@ -0,0 +1,8 @@
+export default function Skills() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center py-2">
+      <h1 className="text-6xl font-bold">My Skills</h1>
+      <p className="mt-3 text-2xl">This is the skills page.</p>
+    </main>
+  )
+}
