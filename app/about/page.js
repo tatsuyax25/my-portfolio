803a81cef52d96e72e30b1f51d048e4df64f439f
Miguel Urena <urena_miguel82@outlook.com>
2024-06-20 10:48:13 -0700

    Updating pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,29 @@
 export default function About() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-bold">About Me</h1>
-      <p className="mt-3 text-2xl">This is the about page.</p>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-100">
+      <section className="max-w-4xl w-full text-center">
+        <h2 className="text-5xl font-bold mb-6 text-gray-8000">About Me</h2>
+        <p className="text-lg text-gray-600 mb-6">
+          Hi, I’m Miguel, and I am a 1st-generation, Latino Software Engineer
+          with an interest in all things artistic and a passion for working with
+          game design and effectiveness. Before beginning my transition to
+          software engineering, I worked in both in-store and online retail as a
+          technical support specialist in e-commerce. Simultaneously, I became a
+          webmaster, taking care of the company’s website by updating and
+          cleaning up the webpage in HTML and CSS to meet clients’ needs, which
+          gave me a keen eye for detail and precision. As I’ve collaborated on
+          apps related to productivity and efficiency, I’m looking forward to
+          further developing my design and coding skills with my passion for
+          gamification to build long-lasting resources for future generations to
+          come.
+        </p>
+        <p className="text-lg text-gray-600 mb-6">
+          In my spare time, I like to watch comedy and action movies or get
+          outdoors such as finding new hiking trails or seeing new sights. While
+          at home, I would play video games and watch anime. I’m also a big
+          sports fan: soccer, hockey, basketball, football, and baseball.
+        </p>
+      </section>
     </main>
-  )
+  );
 }
