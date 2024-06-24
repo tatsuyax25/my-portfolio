012cb1ce216ade067b2795b640122b4fef314225
Miguel Urena <urena_miguel82@outlook.com>
2024-06-24 15:01:41 -0700

    Update's

--- app/page.js
+++ app/page.js
@@ -1,19 +1,7 @@
-import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-10">
       <section className="text-center mb-10">
-        <div className="w-full md:w-1/3 mb-4 md:mb-0 flex justify-center md:justify-start">
-          <div className="relative h-60 w-60 md:h-80 md:w-80 rounded-full overflow-hidden">
-            <Image
-              src="/HeroImage.jpg" // Replace with your image filename
-              alt="Profile"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
-          </div>
-        </div>
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-4 text-gray-800">
           Welcome to My Portfolio
         </h1>
